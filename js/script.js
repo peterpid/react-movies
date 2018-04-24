cd3ee0f803b4b1b2de88383bb718079b975255a0
Piotr Pidlypczak <peterpid@gmail.com>
2018-04-24 19:16:00 +0200

    Add object to initialize DOM structure

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
+var movies = [
+	{
+		title: 'Harry Potter',
+		desc: 'Movie about a wizard boy'
+	},
+	{
+		title: 'Lion King',
+		desc: 'Movie about a lion from savanna'
+	}
+];
+
+var moviesElements = movies.map(function(movie) {
+	return React.createElement('li', {},
+		React.createElement('h2', {}, movie.title),
+		React.createElement('p', {}, movie.desc)
+	);
+});
+
 var element =
 	React.createElement('div', {},
 		React.createElement('h1', {}, 'List of movies'),
-		React.createElement('ul', {},
-			React.createElement('li', {},
-				React.createElement('h2', {}, 'Harry Potter'),
-				React.createElement('p', {}, 'Movie about a wizard boy'),
-			),
-			React.createElement('li', {},
-				React.createElement('h2', {}, 'Lion King'),
-				React.createElement('p', {}, 'Movie about a lion from savanna'),
-			),
-		),
+		React.createElement('ul', {}, moviesElements),
 	);
 ReactDOM.render(element, document.getElementById('app'));
