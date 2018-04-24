668a9293b0112275d2658dedfa92c0d706c2fa37
Piotr Pidlypczak <peterpid@gmail.com>
2018-04-24 20:00:06 +0200

    Add posters and more movies

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
 var movies = [
 	{
-		id:    '1',
-		title: 'Harry Potter',
-		desc:  'Movie about a wizard boy'
+		id:     '1',
+		title:  'Harry Potter',
+		desc:   'Movie about a wizard boy',
+		poster: 'images/harry_potter.jpg'
 	},
 	{
-		id:    '2',
-		title: 'Lion King',
-		desc:  'Movie about a lion from savanna'
+		id:     '2',
+		title:  'Lion King',
+		desc:   'Movie about a lion from savanna',
+		poster: 'images/lion_king.jpg'
+	},
+	{
+		id:     '3',
+		title:  'ET',
+		desc:   'Story about a funny alien',
+		poster: 'images/et.jpg'
+	},
+	{
+		id:     '4',
+		title:  'Rocky',
+		desc:   'Story about a boxer',
+		poster: 'images/rocky.jpg'
 	}
 ];
 
 var moviesElements = movies.map(function(movie) {
 	return React.createElement('li', {key: movie.id},
 		React.createElement('h2', {}, movie.title),
+		React.createElement('img', {src: movie.poster, width: 200}),
 		React.createElement('p', {}, movie.desc)
 	);
 });
