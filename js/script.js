e822b00c933c7f56e6b52e22d83c121de1c888d5
Piotr Pidlypczak <peterpid@gmail.com>
2018-04-24 19:06:07 +0200

    Add list of movies + remove css from index.html

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-var element = React.createElement('div', {}, 'Hello world!');
+var element =
+	React.createElement('div', {},
+		React.createElement('h1', {}, 'List of movies'),
+		React.createElement('ul', {},
+			React.createElement('li', {},
+				React.createElement('h2', {}, 'Harry Potter'),
+				React.createElement('p', {}, 'Movie about a wizard boy'),
+			),
+			React.createElement('li', {},
+				React.createElement('h2', {}, 'Lion King'),
+				React.createElement('p', {}, 'Movie about a lion from savanna'),
+			),
+		),
+	);
 ReactDOM.render(element, document.getElementById('app'));
